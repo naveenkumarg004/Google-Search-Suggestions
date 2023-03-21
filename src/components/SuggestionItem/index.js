7ab362bbede460619481eb75bfd2bfe4a25afd81
naveenkumarg004 <gnaveenkumar166@gmail.com>
2023-03-21 07:57:50 +0000

    first commit

---
+++ src/components/SuggestionItem/index.js
@@ -0,0 +1,23 @@
+import './index.css'
+
+const SuggestionItem = props => {
+  const {suggestion, onFullSuggestion} = props
+
+  const onClickFullSuggestion = () => {
+    onFullSuggestion(suggestion)
+  }
+
+  return (
+    <li className="suggestion-item">
+      <p className="suggestion-name">{suggestion}</p>
+      <img
+        className="arrow-image"
+        src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+        alt="arrow"
+        onClick={onClickFullSuggestion}
+      />
+    </li>
+  )
+}
+
+export default SuggestionItem
